0b4c0658774fed30f58e19334a69626f382da51c
davitlabadze <labadzedato18@gmail.com>
2021-11-11 20:59:58 +0400

    style(translatebutton): text color

--- resources/views/components/translatebutton.blade.php
+++ resources/views/components/translatebutton.blade.php
@@ -1,5 +1,5 @@
 @foreach (config('app.available_locales') as $locale)
-    <div class="w-12 h-12 px-3 py-3 text-sm text-center text-black @if (app()->getLocale() != $locale) bg-gray-50 @endif bg-transparent  border-2 rounded-full cursor-pointer">
+    <div class="w-12 h-12 px-3 py-3 text-sm text-center  @if (app()->getLocale() != $locale) bg-gray-50 @else text-white @endif bg-transparent  border-2 rounded-full cursor-pointer">
         <a href="{{ request()->url() }}?language={{ $locale }} ">{{ strtoupper($locale) }}</a>
     </div>
 @endforeach
