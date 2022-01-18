49e2a8c929ffc6e474f287576a3b2da096a36e2c
davitlabadze <labadzedato18@gmail.com>
2022-01-18 12:32:48 +0400

    fix(import): remove unused import

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
 class AdminController extends Controller
