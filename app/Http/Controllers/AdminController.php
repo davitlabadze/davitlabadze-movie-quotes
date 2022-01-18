198dc17bef452cae81d625b7392d980e849d8ada
davitlabadze <labadzedato18@gmail.com>
2022-01-18 13:13:57 +0400

    fix(admincontroller and web):delete comment that give nothing

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,22 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreAdminRequest;
 use App\Models\Post;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
 class AdminController extends Controller
 {
-    /**
-     * login view function
-     */
+
     public function login()
     {
         return view('backend/login');
     }
 
-    /**
-     * submit login function
-     */
     public function submitLogin(StoreAdminRequest $request)
     {
         $attributes = $request->validated();
@@ -34,14 +28,12 @@ class AdminController extends Controller
         return redirect()->route('dashboard');
     }
 
-    //dashboard
     public function dashboard()
     {
         $posts = Post::orderBy('id', 'desc')->paginate(5);
         return view('backend.dashboard', ['posts'=>$posts]);
     }
 
-    //Logout
     public function destroy()
     {
         session()->forget(['adminData']);
