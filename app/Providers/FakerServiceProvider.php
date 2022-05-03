5edbf4cb21be98cad366ffa871817144e6e3bce7
davitlabadze <labadzedato18@gmail.com>
2022-05-03 22:57:25 +0400

    feat: custom faker service provider

---
+++ app/Providers/FakerServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Faker\FrameworkProvider;
+use Faker\Factory;
+use Faker\Generator;
+
+class FakerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Generator::class, function () {
+            $faker = Factory::create();
+            $faker->addProvider(new FrameworkProvider($faker));
+            return $faker;
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
