f8cbfaae30e47dc547213c8bae64f1af56b65c0c
davitlabadze <labadzedato18@gmail.com>
2022-05-04 01:03:13 +0400

    feat: create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Movie;
+use App\Models\Quote;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Movie::truncate();
+        Quote::truncate();
+        Movie::create([
+            'movie'=> [
+                'en' => 'The Godfather',
+                'ka' => 'ნათლია'
+            ]
+        ]);
+
+        Quote::create([
+            'movie_id' => '1',
+            'quote'=>[
+                'en' => "I'm gonna make him an offer he can't refuse.",
+                'ka' => "მე მას შეთავაზებას გავუკეთებ, რომელზეც უარს ვერ იტყვის"
+            ],
+            'thumbnail' => 'thumbnails/thegodfather.jpg'
+        ]);
+
+        Quote::create([
+            'movie_id' => '1',
+            'quote'=>[
+                'en' => "I Know It Was You, Fredo. You Broke My Heart. You Broke My Heart!",
+                'ka' => "ვიცი, რომ შენ იყავი, ფრედო. გული გამიტეხე. გული გამიტეხე!"
+            ],
+            'thumbnail' => 'thumbnails/the-godfather-part-II.webp'
+        ]);
+
+        Quote::create([
+            'movie_id' => '1',
+            'quote'=>[
+                'en' => "Time erodes gratitude more quickly than it does beauty!",
+                'ka' => "დრო უფრო სწრაფად ანადგურებს მადლიერებას, ვიდრე სილამაზეს!"
+            ],
+            'thumbnail' => 'thumbnails/godfatherofficial.jpg'
+        ]);
     }
 }
